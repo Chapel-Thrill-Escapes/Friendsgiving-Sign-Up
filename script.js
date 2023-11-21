53f59a936deb85b4d3a41e2bbcd6d6ca5b3fe73d
Riley Harper <riley.harper@unc.edu>
2023-11-21 13:05:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
+// Example names for autocomplete. Replace with actual names or fetch from a source.
+const names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Hannah", "Ivy", "Jack"];
+
+function filterNames() {
+    let input = document.getElementById('nameInput');
+    let filter = input.value.toUpperCase();
+    let suggestions = document.getElementById('nameSuggestions');
+    suggestions.innerHTML = '';
+
+    if (!filter) {
+        suggestions.style.display = "none";
+        return;
+    }
+
+    names.forEach(function(name) {
+        if (name.toUpperCase().indexOf(filter) > -1) {
+            let div = document.createElement("div");
+            div.innerHTML = name;
+            div.onclick = function() {
+                input.value = name;
+                suggestions.style.display = "none";
+            };
+            suggestions.appendChild(div);
+        }
+    });
+    suggestions.style.display = "block";
+}
+
 function addFood() {
+    let name = document.getElementById('nameInput').value;
     let food = document.getElementById('foodInput').value;
-    if (food) {
+    if (food && name) {
         google.script.run.withSuccessHandler(function(response) {
             console.log(response);
-        }).addFoodToSheet(food);
+        }).addFoodToSheet(food, name);
     }
     document.getElementById('foodInput').value = '';
+    document.getElementById('nameInput').value = '';
+    document.getElementById('nameSuggestions').style.display = "none";
 }
