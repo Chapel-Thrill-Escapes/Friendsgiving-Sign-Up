1dc6f7c368e70efe950c7967d82dc8e34d6ba4bb
Riley Harper <riley.harper@unc.edu>
2023-11-21 12:32:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-// script.js
-document.getElementById('foodInput').addEventListener('input', function(e) {
-    // Autocomplete functionality
-    // You'll need a list of top 100 Thanksgiving foods
-    // For simplicity, let's say you have it in an array `thanksgivingFoods`
-    let input = e.target.value;
-    let suggestions = thanksgivingFoods.filter(food => food.toLowerCase().startsWith(input.toLowerCase()));
-    
-    // Display these suggestions (you might want to create a dropdown or similar)
+// Initialize the list from LocalStorage
+document.addEventListener('DOMContentLoaded', function() {
+    if (localStorage.getItem('foodList')) {
+        document.getElementById('signupList').innerHTML = localStorage.getItem('foodList');
+    }
 });
 
 function addFood() {
-    // Function to add food to the list
     let food = document.getElementById('foodInput').value;
     let list = document.getElementById('signupList');
-    list.innerHTML += '<p>' + food + ' - Your Name</p>'; // Replace 'Your Name' with the user's name if available
+    let newItem = '<p>' + food + ' - Your Name</p>'; // Replace 'Your Name' with the user's name if available
+
+    list.innerHTML += newItem;
+
+    // Store the updated list in LocalStorage
+    localStorage.setItem('foodList', list.innerHTML);
 
     // Clear the input field
     document.getElementById('foodInput').value = '';
 }
 
-// Example array, replace it with real data
+// Example array for autocomplete, replace it with real data
 const thanksgivingFoods = ["Turkey", "Stuffing", "Cranberry Sauce", ...]; // top 100 foods
+
+document.getElementById('foodInput').addEventListener('input', function(e) {
+    // Implement autocomplete functionality here
+});
