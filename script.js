bb5cb81bdcb5cee4273e64ed18af6056bb1331df
Riley Harper <riley.harper@unc.edu>
2023-11-21 13:02:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-// Initialize the list from LocalStorage
-document.addEventListener('DOMContentLoaded', function() {
-    if (localStorage.getItem('foodList')) {
-        document.getElementById('signupList').innerHTML = localStorage.getItem('foodList');
-    }
-});
-
 function addFood() {
     let food = document.getElementById('foodInput').value;
-    let list = document.getElementById('signupList');
-    let newItem = '<p>' + food + ' - Your Name</p>'; // Replace 'Your Name' with the user's name if available
-
-    list.innerHTML += newItem;
-
-    // Store the updated list in LocalStorage
-    localStorage.setItem('foodList', list.innerHTML);
-
-    // Clear the input field
+    if (food) {
+        google.script.run.withSuccessHandler(function(response) {
+            console.log(response);
+        }).addFoodToSheet(food);
+    }
     document.getElementById('foodInput').value = '';
 }
-
-// Example array for autocomplete, replace it with real data
-const thanksgivingFoods = ["Turkey", "Stuffing", "Cranberry Sauce", ...]; // top 100 foods
-
-document.getElementById('foodInput').addEventListener('input', function(e) {
-    // Implement autocomplete functionality here
-});
